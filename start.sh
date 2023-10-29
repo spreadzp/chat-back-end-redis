fbbc2372f9c2c2f232d96a6e0ecc017f4cf66b57
Paul <spread2009@gmail.com>
2023-10-30 00:49:55 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t your-image-name:your-image-tag .
-
-# Run the Docker container
-docker run -d -p 3000:3000 -p 6379:6379 --name your-container-name your-image-name:your-image-tag
